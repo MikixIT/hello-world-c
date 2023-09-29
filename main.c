9ea106540e111dfd186bd2dce7f4895dc88c40bb
MikixIT <michael.46@live.it>
2023-09-29 16:01:36 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  first-c-software
+//
+//  Created by Michael Torres on 29/09/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World by MikixIT in C!\n");
+    return 0;
+}
